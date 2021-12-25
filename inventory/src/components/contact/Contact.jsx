c86f6e54e52b82ced1a16059c97e42400c0161d0
daniel <d@nielhe.com>
2021-12-24 23:31:50 -0800

    10vw alignment marleft

--- inventory/src/components/contact/Contact.jsx
+++ inventory/src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <div className = "contact" id="contact">
             <div className="content">
-                <div className="tag">contact</div>
+                <div className="tag">Contact</div>
                 <div className="interested">Interested?</div>
                 <div className="emailbox"></div>
             </div>
