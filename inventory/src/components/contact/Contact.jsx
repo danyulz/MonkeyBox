a79b59830e1134b70fa93bb9dfb7fb07a3bfc538
daniel <d@nielhe.com>
2022-01-02 20:47:21 -0800

    lots

--- inventory/src/components/contact/Contact.jsx
+++ inventory/src/components/contact/Contact.jsx
@@ -6,7 +6,12 @@ export default function Contact() {
             <div className="content">
                 <div className="tag">Contact</div>
                 <div className="interested">Interested?</div>
-                <div className="emailbox"></div>
+                <div className="emailbox">
+                    <div className="mail">
+                        <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M.254 10.188H15.12V.438H.254v9.75zm2.403-8.635h10.075L7.72 4.923l-5.062-3.37zm-1.287.484l6.352 4.228 6.283-4.222v7.03H1.37V2.037z" fill="#FFFFFF" fill-rule="evenodd"></path></svg>
+                    </div>
+                    <div className="email">d@nielhe.com</div>
+                </div>
             </div>
         </div>
     )
