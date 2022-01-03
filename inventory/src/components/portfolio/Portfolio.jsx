b2cde5bbc1ed9748d5cf97308616c0a28d0778f7
daniel <d@nielhe.com>
2022-01-03 00:06:05 -0800

    project boxes and diaglines

--- inventory/src/components/portfolio/Portfolio.jsx
+++ inventory/src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import "./portfolio.scss"
+import Neigh from "../../assets/Neigh.png"
 
 export default function Portfolio() {
     return (
@@ -8,8 +9,18 @@ export default function Portfolio() {
                 <div>&&nbsp;Works</div>
             </div>
             <div className="object">
-                <div className="left"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <div className="project crossed">
+                        <div className="img">
+                            <img className="neighImg" src={Neigh} alt="Neigh App Logo"/>
+                        </div>
+                    </div>
+                    <div className="project crossed right"></div>
+                </div>
+                <div className="right">
+                    <div className="project crossed"></div>
+                    <div className="project crossed right"></div>    
+                </div>
             </div>
         </div>
     )
